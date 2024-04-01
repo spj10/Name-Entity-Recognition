a69dda3a144fd20ba3681e948d2e56cd128d66b3
Sachin Jadhav <sp.jadhav10@gmail.com>
2024-04-01 21:04:34 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Sachin",
+    author_email="sp.jadhav10@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
